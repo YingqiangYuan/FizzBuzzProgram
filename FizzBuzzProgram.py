80119dc73cda8eb63964d427879f9eab556bd52f
YingqiangYuan <yingqiang.yuan@gmail.com>
2025-02-12 03:53:14 -0800

    Update FizzBuzzProgram.py
    Formalize the comment. Delete unnecessary function. Generate shorter functioning code.

--- FizzBuzzProgram.py
+++ FizzBuzzProgram.py
@@ -1,22 +1,34 @@
-## FizzBuzzProgram
-
-def getCase(num):
-    result = 0
-    if num%3==0 :  
-        result += 1
-    if num%5==0:
-        result += 2
-    return result
+#*********************************************************************************#
+# FILE: FizzBuzz Problem                                                          #
+#                                                                                 #
+# DESCRIPTION: This program prints each number from 1 to 100 on a new line.       #
+# For each multiple of 3, print "Fizz" instead of the number.                     #
+# For each multiple of 5, print "Buzz" instead of the number.                     #
+# For each multiples of both 3 and 5, print "FizzBuzz" instead of the number.     #
+# Otherwise, print the number.                                                    #
+#                                                                                 #
+# DEVELOPER: Yingqiang Yuan                                                       #
+# DEVELOPER PHONE: +1 (509) 288-2111                                              #
+# DEVELOPER EMAIL: yingqiang.yuan@gmail.com                                       #
+#                                                                                 #
+# VERSION: 1.0                                                                    #
+# CREATED DATE-TIME: 20250207-11:44 Pacfic Time Zone USA                          #
+#                                                                                 #
+# VERSION: 1.1                                                                    #
+# REVISION DATE-TIME: 20250210-14:18                                              #
+# DEVELOPER MAKING CHANGE: First_name Last_name                                   #
+# DEVELOPER MAKING CHANGE: PHONE: +1 (XXX) XXX-XXXX                               #
+# DEVELOPER MAKING CHANGE: EMAIL: first.last@email.com                            #
+#*********************************************************************************#
 
 for i in range(1, 101):
-    myCase = getCase(i)
-    
-    match myCase:
-        case 0:
-            print(i)
-        case 1: 
-            print("Fizz")
-        case 2: 
-            print("Buzz")
-        case 3:
-            print("FizzBuzz")
+    outputWord = ""
+
+    if i%3==0:
+        outputWord += "Fizz"
+    if i%5==0:
+        outputWord += "Buzz"
+    if outputWord == "":
+        outputWord = str(i)
+
+    print(outputWord)
